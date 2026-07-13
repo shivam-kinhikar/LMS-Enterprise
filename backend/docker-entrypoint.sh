f55711e6e89ee4aa1fec30d8c0eb3827637470df
shivam kinhikar <shivamkinhikar13@gmail.com>
2026-07-13 20:48:10 +0530

    added docker

---
+++ backend/docker-entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Exit on error
+set -e
+
+# Replace the default Apache port (80) with the port provided by Render ($PORT)
+sed -i "s/80/$PORT/g" /etc/apache2/sites-available/000-default.conf /etc/apache2/ports.conf
+
+# Run migrations if database is connected
+# php artisan migrate --force
+
+# Execute the main container command (apache2-foreground)
+exec "$@"
